23f60f0f36c2899d91098a9fb9b52d35e06a9f38
Katerina-il <kkaterina.iln@gmail.com>
2024-12-06 15:06:19 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word, *other_words):
+    same_words =[]
+    print(type(other_words))
+    for i in other_words:
+       same_words.append(i)
+
+    print(same_words)
+
+
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
